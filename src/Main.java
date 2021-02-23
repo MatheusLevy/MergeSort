fcba7b99dde1f99128041036ed3cb232859100e6
Matheus Levy <levybessa2001@hotmail.com>
2021-02-23 17:37:49 -0300

    Att Merge

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 
 public class Main {
     public static void main(String[] args){
+        int[] arr = new int[5];
+        arr[0]= 3;
+        arr[1]= 2;
+        arr[2]= 9;
+        arr[3]= 12;
+        arr[4]= 78;
+        MergeSort.mergesort(arr,0,5);
+        for (int i=0;i<arr.length;i++)
+            System.out.println("Vetor["+i+"]= " +arr[i]);
+
 
     }
 
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.SysexMessage;
 import java.util.ArrayList;
 
 public class MergeSort {
@@ -9,7 +10,7 @@ public class MergeSort {
             meio = (int) Math.floor((inicio+fim)/2); // Calcula onde fica o meio do vetor
             mergesort(vetor,inicio,meio);            // Chamada recursiva do inicio do vetor até a metade
             mergesort(vetor,meio+1,fim);      // Chamada recursiva da 2ª parte do vetor (meio+1) até o fim
-            //merge(vetor,inicio,meio,fim);
+            merge(vetor,inicio,meio,fim);
         }
     }
 
@@ -18,7 +19,6 @@ public class MergeSort {
         int cont1 = inicio; // Contador da 1ª parte
         int cont2= meio+1; // Contador da 2ª parte
         int tamanho = fim-inicio+1; // tamanho do vetor enviado para função.
-
         //fim1 e fim2 correspondem a 2 possibilidades de saida do if
         //Sendo fim1 quando o contador cont1 passar do meio do vetor. Pois o primeiro vetor corresponde somente a primeira parte do vetor.
         //E fim2 quando o contador cont2 passar do fim do vetor. Pois o segundo vetor corresponde  somente a segunda parte do vetor.
@@ -26,8 +26,8 @@ public class MergeSort {
 
         //O tamanho de vetorP1 é meio, pois ele é vai só até o meio do vetor
         //O tamanho do vetorP2 é fim - cont2, pois fim-cont2 corresponde ao tamanho do meio+1 até o fim do vetor.
-        int[] vetorP1 = new int[meio]; // Vetor correspondente a primeira parte do vetor: do inicio até a meio;
-        int[] vetorP2 = new int[fim-cont2]; // Vetor correspondente a segunda parte do vetor: do meio+1 até o fim;
+        int[] vetorP1 = new int[meio+1]; // Vetor correspondente a primeira parte do vetor: do inicio até a meio;
+        int[] vetorP2 = new int[(fim-cont1)+1]; // Vetor correspondente a segunda parte do vetor: do meio+1 até o fim;
 
         /* Vamos dividir o vetor recebido em 2 vetores, os vetores vetorP1 e o vetor, a primeira parte do vetor recebido
            que corresponde aos valores do inicio até o meio serão armazenados em vetorP1 e a segunda parte que corresponde
@@ -41,7 +41,7 @@ public class MergeSort {
         int auxP2=0; //Contador no vetorP2 que é o sub arrey de que começou em cont2 e termina em fim.
         for(int i=0; i<tamanho;i++){
             if(!fim1 && !fim2){ // Se nenhum contador tiver passado do seu limite
-                if(vetorP1[cont1]<vetorP2[auxP2]) {             //Compara as duas posições dos dois vetores. O merge deve colocar
+                if(vetorP1[cont1]<=vetorP2[auxP2]) {             //Compara as duas posições dos dois vetores. O merge deve colocar
                     vetor[i] = vetorP1[cont1];                  //no vetor o menor e depois o maior. Ele compara as posições dos subvetores
                     cont1++;                                    //vetorP1 e vetorP2 para ordenar os dois subarrays em um unico array que
                 }else{                                          // é o array recebido na função
@@ -58,14 +58,20 @@ public class MergeSort {
                 if(auxP2>fim-cont2) //fim-cont2 é o tamanho maximo do contador aauxP2, pois determina a quantidade de elementos
                     fim2 = true;    // que existem do meio+1 até o fim do vetor.
             }else{ // caso ele não tenha entrado no if vamos verificar se foi por causa da condição fim1 ou fim2.
-                if(!fim1){
-
+                if(!fim1) { //Se fim1 for false então negar fim1 dará true entrando no if. Logo, entrar nesse if siginifica que
+                    //fim1 = false, logo fim2 = true. Sendo assim sobrou elementos no vetorP1.
+                    while (cont1 < meio) {
+                        vetor[cont1] = vetorP1[cont1];
+                        cont1++;
+                    }
+                }else{ // saiu pois fim1 = true e fim2 = false;
+                    while(auxP2<fim - cont2){
+                        vetor[auxP2] = vetorP2[auxP2];
+                        auxP2++;
+                    }
                 }
             }
         }
-        //Vetores Temporarios
-
-
     }
 }
 
