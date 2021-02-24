227b561e1af067fc17828a736a0cc0a539bca047
Matheus Levy <levybessa2001@hotmail.com>
2021-02-23 23:28:06 -0300

    Refeito Merge

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,10 @@
 
 public class Main {
     public static void main(String[] args){
-        int[] arr = new int[5];
-        arr[0]= 3;
-        arr[1]= 2;
-        arr[2]= 9;
-        arr[3]= 12;
-        arr[4]= 78;
-        MergeSort.mergesort(arr,0,5);
+        int[] arr = {3,5,4,1,9,6,7,2,10};
+        MergeSort.mergesort(arr,0,arr.length);
         for (int i=0;i<arr.length;i++)
             System.out.println("Vetor["+i+"]= " +arr[i]);
-
 
     }
 
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -6,72 +6,69 @@ public class MergeSort {
     public static void mergesort(int[] vetor, int inicio, int fim){
 
         int meio;
-        if(inicio<fim){
-            meio = (int) Math.floor((inicio+fim)/2); // Calcula onde fica o meio do vetor
+        if(inicio<fim-1){
+            meio = (inicio+fim)/2; // Calcula onde fica o meio do vetor
             mergesort(vetor,inicio,meio);            // Chamada recursiva do inicio do vetor até a metade
-            mergesort(vetor,meio+1,fim);      // Chamada recursiva da 2ª parte do vetor (meio+1) até o fim
+            mergesort(vetor,meio,fim);// Chamada recursiva da 2ª parte do vetor (meio+1) até o fim
             merge(vetor,inicio,meio,fim);
+
         }
     }
+    //Função merge padrão. Não exclui elementos iguais
+    public static void merge(int[] vetor, int inicio, int meio, int fim) {
+        //Cria um vetor temporario para trabalharmos a ordenação.
+        //O vetor temporario tem o mesmo tamanho do vetor orginal sendo uma copia do mesmo ordenado.
 
-    public static void merge(int[] vetor, int inicio, int meio, int fim){
+        int vetorTemporario[] = new int[fim - inicio];
 
-        int cont1 = inicio; // Contador da 1ª parte
-        int cont2= meio+1; // Contador da 2ª parte
-        int tamanho = fim-inicio+1; // tamanho do vetor enviado para função.
-        //fim1 e fim2 correspondem a 2 possibilidades de saida do if
-        //Sendo fim1 quando o contador cont1 passar do meio do vetor. Pois o primeiro vetor corresponde somente a primeira parte do vetor.
-        //E fim2 quando o contador cont2 passar do fim do vetor. Pois o segundo vetor corresponde  somente a segunda parte do vetor.
-        boolean fim1=false, fim2=false;
-
-        //O tamanho de vetorP1 é meio, pois ele é vai só até o meio do vetor
-        //O tamanho do vetorP2 é fim - cont2, pois fim-cont2 corresponde ao tamanho do meio+1 até o fim do vetor.
-        int[] vetorP1 = new int[meio+1]; // Vetor correspondente a primeira parte do vetor: do inicio até a meio;
-        int[] vetorP2 = new int[(fim-cont1)+1]; // Vetor correspondente a segunda parte do vetor: do meio+1 até o fim;
-
-        /* Vamos dividir o vetor recebido em 2 vetores, os vetores vetorP1 e o vetor, a primeira parte do vetor recebido
-           que corresponde aos valores do inicio até o meio serão armazenados em vetorP1 e a segunda parte que corresponde
-           do meio+1 até o fim serão armazenados em vetorP2.
-         */
-        for(int i=0;i<cont1;i++)
-            vetorP1[i] = vetor[i];
-        for(int i=cont2;i<fim;i++)
-            vetorP2[i] = vetor[i];
-
-        int auxP2=0; //Contador no vetorP2 que é o sub arrey de que começou em cont2 e termina em fim.
-        for(int i=0; i<tamanho;i++){
-            if(!fim1 && !fim2){ // Se nenhum contador tiver passado do seu limite
-                if(vetorP1[cont1]<=vetorP2[auxP2]) {             //Compara as duas posições dos dois vetores. O merge deve colocar
-                    vetor[i] = vetorP1[cont1];                  //no vetor o menor e depois o maior. Ele compara as posições dos subvetores
-                    cont1++;                                    //vetorP1 e vetorP2 para ordenar os dois subarrays em um unico array que
-                }else{                                          // é o array recebido na função
-                    vetor[i]=vetorP2[auxP2];
-                    auxP2++;
-                }
-
-                /*Precisamos verificar sempre se nenhum dos nosso 2 contadores passaram do limeite. Ou seja, se cont1
-                  passou de meio, pois meio+1 já não está definido no tamanho do vetorP1 que só vai até meio. E auxP2 começa
-                  em 0 e vai até
-                */
-                if(cont1>meio)
-                    fim1 = true;
-                if(auxP2>fim-cont2) //fim-cont2 é o tamanho maximo do contador aauxP2, pois determina a quantidade de elementos
-                    fim2 = true;    // que existem do meio+1 até o fim do vetor.
-            }else{ // caso ele não tenha entrado no if vamos verificar se foi por causa da condição fim1 ou fim2.
-                if(!fim1) { //Se fim1 for false então negar fim1 dará true entrando no if. Logo, entrar nesse if siginifica que
-                    //fim1 = false, logo fim2 = true. Sendo assim sobrou elementos no vetorP1.
-                    while (cont1 < meio) {
-                        vetor[cont1] = vetorP1[cont1];
-                        cont1++;
-                    }
-                }else{ // saiu pois fim1 = true e fim2 = false;
-                    while(auxP2<fim - cont2){
-                        vetor[auxP2] = vetorP2[auxP2];
-                        auxP2++;
-                    }
-                }
+        //O cont1 é o contador que irá percorrer o primeiro subvetor, que está dentro do vetorTemporario.Sendo do inicio até o meio.
+        //O cont2 é o contador que irá percorrer o segundo subvetor, que está dentro do vetorTemporario. Sendo do meio+1 até o fim.
+        int cont1 = inicio;
+        int cont2 = meio;
+        // i corresponde a um contador para a posição do vetorTemporario onde será inserido o valor;
+        int i = 0;
+        //O while vai durar enquanto cont1 for não chegar em meio+1 que é o primeiro elemento do segundo subvetor.
+        //e cont2 não chegar no fim do segundo subVetor que como estamos fazendo tudo dentro do vetorTemporario
+        //corresponde ao fim do vetor.
+        while (cont1 < meio && cont2 < fim) {
+            if(vetor[cont1]<=vetor[cont2]){
+             //   System.out.println("vetor["+cont1+"]="+vetor[cont1]+"<="+"vetor["+cont2+"]="+vetor[cont2]);
+                vetorTemporario[i] = vetor[cont1]; //O vetorTemporario recebe o valor da posição cont1 pois é a menor
+                i++; //Incrementamos o contador de posição do vetorTemporario.
+                cont1++; //Incrementamos o contador de posição do primeiro subvetor já que o valor foi adicionado ao vetorTemporario.
+            }else{
+                //O mesmo do acima
+                vetorTemporario[i]=vetor[cont2];
+                i++;
+                cont2++; //Incrementamso o contador de posição do segundo subvetor
             }
         }
+
+        //Colocando os valores que sobraram no primeiro subarray
+        //cont1<meio+1 é o mesmo que dizer que que existem elementos entre inicio e meio+1 que sobraram.
+        while(cont1<meio){
+            vetorTemporario[i] = vetor[cont1];
+            i++;
+            cont1++;
+        }
+        //Colocando os valores que sobraram no segundo subarray
+        //cont2<fim é o mesmo que dizer que existem elementos entre meio+1 e fim que sobraram
+        while(cont2<fim){
+            vetorTemporario[i] = vetor[cont2];
+            i++;
+            cont2++;
+        }
+
+        //Vamos transferir os valores do vetorTemporario para o vetor original
+        //Para vetores que iniciam em 0 k também será 0 obviamente.
+        //Mas se podemos ter um inicio diferente de 0
+        int j;
+        int k;
+        for(j=0,k=inicio;k<fim;j++,k++){
+            vetor[k]=vetorTemporario[j];
+
+        }
     }
+
 }
 
